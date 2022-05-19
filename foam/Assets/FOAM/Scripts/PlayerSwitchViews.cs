4f0a2a1a98cb72fe0e23286c91a5450773c1a668
vladimir svistunov <vover@mail.ru>
2022-05-19 21:30:09 +0300

    статичный класс переключение камер, R! PlayerSwitchViews

--- foam/Assets/FOAM/Scripts/PlayerSwitchViews.cs
+++ foam/Assets/FOAM/Scripts/PlayerSwitchViews.cs
@@ -4,17 +4,34 @@ using StarterAssets;
 
 public class PlayerSwitchViews : MonoBehaviour
 {
+    //cameras
     [SerializeField] private CinemachineVirtualCamera firstPersonCam;
     [SerializeField] private CinemachineVirtualCamera thirdPersonCam;
-    [SerializeField] private GameObject playerObject;
-    [SerializeField] private ShaderController shaderController;
-
+    [SerializeField] private CinemachineVirtualCamera isometricCam;
+    
+    //camera roots
     [SerializeField] private Transform thirdPersonCameraRoot;
 
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private ShaderController shaderController;
     [SerializeField] private Distance distaince;
 
     private bool isFirstPerson;
     private ThirdPersonController thirdPersonController;
+
+    private void OnEnable()
+    {
+        CameraSwitcher.Register(firstPersonCam);
+        CameraSwitcher.Register(thirdPersonCam);
+        CameraSwitcher.Register(isometricCam);
+
+    }
+    private void OnDisable()
+    {
+        CameraSwitcher.Unregister(firstPersonCam);
+        CameraSwitcher.Unregister(thirdPersonCam);
+        CameraSwitcher.Unregister(isometricCam);
+    }
 
     private void Start()
     {
@@ -33,29 +50,15 @@ public class PlayerSwitchViews : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (isFirstPerson)
+            if (isFirstPerson) //если щас изометрия
             {
-                firstPersonCam.gameObject.SetActive(false);
-                thirdPersonCam.gameObject.SetActive(true);
-                isFirstPerson = false;
-                shaderController.enabled = true;
-
-                shaderController.StartChangeViewEffect(true);
-                
-                Debug.Log("to third person");
+                ChangeToThirdView(); //изменить на вид от третьего лица
+                isFirstPerson = !isFirstPerson;
             }
             else
             {
-                
-                //firstPersonCameraRoot.rotation = Quaternion.Euler(Vector3.back);
-                firstPersonCam.gameObject.SetActive(true);
-                thirdPersonCam.gameObject.SetActive(false);
-                isFirstPerson = true;
-
-                shaderController.enabled = true;
-                shaderController.StartChangeViewEffect(false);
-
-                Debug.Log("to first person");
+                ChangeToIsometricView();
+                isFirstPerson = !isFirstPerson;
             }
         }
     }
@@ -70,6 +73,37 @@ public class PlayerSwitchViews : MonoBehaviour
             thirdPersonController.enabled = false;
         }
         
+    }
+
+    private void ChangeToFirsView()
+    {
+        CameraSwitcher.SwitchCamera(firstPersonCam);
+
+        shaderController.enabled = true;
+
+        shaderController.StartChangeViewEffect(false);
+
+        Debug.Log("to first person");
+    }
+
+    private void ChangeToThirdView()
+    {
+        CameraSwitcher.SwitchCamera(thirdPersonCam);
+
+        shaderController.enabled = true;
+
+        shaderController.StartChangeViewEffect(true);
+
+        Debug.Log("to third person");
+    }
+
+    private void ChangeToIsometricView()
+    {
+        
+    }
+    public void ChangeToStaticThirdPerson(CinemachineVirtualCamera cam)
+    {
+
     }
 
 }
