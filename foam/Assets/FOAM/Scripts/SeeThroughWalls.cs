7b4367bf32920a39905e4b201fb6f2456c1ba3fb
vladimir svistunov <vover@mail.ru>
2022-05-20 17:41:59 +0300

    импорт dotween, R! SeeThroughWalls

--- foam/Assets/FOAM/Scripts/SeeThroughWalls.cs
+++ foam/Assets/FOAM/Scripts/SeeThroughWalls.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class SeeThroughWalls : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class SeeThroughWalls : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] private LayerMask mask;
     [SerializeField] private Transform playerPosition;
+    private float currentSize = 0f;
+    private float desiredSize = 1f;
+    private float duration = 0.3f;
+    private bool isHit;
 
 
     void Update()
@@ -18,12 +23,47 @@ public class SeeThroughWalls : MonoBehaviour
         var dir = cam.transform.position - playerPosition.position;
         var ray = new Ray(playerPosition.position, dir.normalized);
 
-        if (Physics.Raycast(ray, 3000, mask))
-            wallMaterial.SetFloat(SizeID, 1);
-        else
-            wallMaterial.SetFloat(SizeID, 0);
+        var rayHit = Physics.Raycast(ray, 3000, mask);
+        if ( rayHit && !isHit)
+        {
+            isHit = true;
+            DOTween.To(() => currentSize, x => currentSize = x, desiredSize, duration);
+            StartCoroutine(SetWallMaterialSize(true));
+            //Debug.Log("hit obj");
+        }   
+
+        if(!rayHit && isHit)
+        {
+            isHit = false;
+            DOTween.To(() => currentSize, x => currentSize = x, 0, duration);
+            StartCoroutine(SetWallMaterialSize(false));
+            //Debug.Log("not hit obj");
+        }
+            
 
         var view = cam.WorldToViewportPoint(playerPosition.position);
         wallMaterial.SetVector(PosID, view);
+    }
+
+    private IEnumerator SetWallMaterialSize(bool isIncrease)
+    {
+        if (isIncrease)
+        {
+            while (currentSize < desiredSize  && isHit)
+            {
+                wallMaterial.SetFloat(SizeID, currentSize);
+                Debug.Log("Coroutine working");
+                yield return null;
+            }
+        }
+        else
+        {
+            while (currentSize > 0.05f && !isHit)
+            {
+                wallMaterial.SetFloat(SizeID, currentSize);
+                Debug.Log("Coroutine working");
+                yield return null;
+            }
+        }
     }
 }
